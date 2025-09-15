b730daae492206cb402fd7ad936b1b4b837e983e
Karthick <karthickraja2422@gmail.com>
2025-09-15 12:27:30 -0700

    Passing commit

--- src/test/java/pageobjects/Loginpage.java
+++ src/test/java/pageobjects/Loginpage.java
@@ -1,13 +1,13 @@
 package pageobjects;
 
-import java.util.ArrayList;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class Loginpage extends Basepage {
@@ -22,8 +22,8 @@ public class Loginpage extends Basepage {
 	@FindBy(xpath = "//div[@class = \"tUxRFH\"]//div[@class = \"col col-7-12\"]/div[text() = \"Apple iPhone 16 Plus (White, 512 GB)\"]") WebElement productname;
 	@FindBy(xpath = "//div[contains(@class,'Nx9bqj') and contains(@class,'CxhGGd')]") WebElement productprice;
 	@FindBy(xpath = "//button[@class = \"QqFHMw vslbG+ _3Yl67G _7Pd1Fp\"]") WebElement buynowbtn;
-	@FindBy(xpath = "//input[@type = \"text\"]") WebElement emailfield ;
-	@FindBy(xpath = "//button[@type = \"submit\"]") WebElement continuebtn ;
+	@FindBy(xpath = "//input[@type = 'text']") WebElement emailfield ;
+	@FindBy(xpath = "//button[@type = 'submit']") WebElement continuebtn ;
 	@FindBy(xpath = "//input[@type='text' and @maxlength='6']") WebElement enterotp;
 
 	
@@ -62,14 +62,18 @@ public class Loginpage extends Basepage {
 		System.out.println("Signin page displayed");
 	}
 	
-	public void gettitle() {
+	public void gettitle() throws InterruptedException {
+		Thread.sleep(5);
 		String title = driver.getTitle();
 		System.out.println("Title of the page: "+ title);
 
 	}
 	
-	public void emailenter() {
+	public void emailenter() throws InterruptedException {
+		PageFactory.initElements(driver, this);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type = 'text']")));
 		wait.until(ExpectedConditions.elementToBeClickable(emailfield));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", emailfield);
 		emailfield.sendKeys("karthikijay234@gmail.com");
 		continuebtn.click();
 		 wait.until(ExpectedConditions.refreshed(
